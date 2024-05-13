1fef223246aee05342b0d05008eb21719874c728
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2024-05-13 08:38:43 +0200

    import sub-components into the app

--- src/app/components/courses-page/courses-page.component.ts
+++ src/app/components/courses-page/courses-page.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-courses-page',
   templateUrl: './courses-page.component.html',
-  styleUrl: './courses-page.component.scss'
+  styleUrl: './courses-page.component.scss',
 })
-export class CoursesPageComponent {
-
-}
+export class CoursesPageComponent {}
