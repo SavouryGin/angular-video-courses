b57d8fa8549446bccb167514c5439cb4e22e5527
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2024-06-22 15:39:57 +0200

    feat: add authGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CoursesPageComponent } from './components/courses-page/courses-page.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
+import { authGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/courses', pathMatch: 'full' },
-  { path: 'courses', component: CoursesPageComponent },
+  {
+    path: 'courses',
+    component: CoursesPageComponent,
+    canActivate: [authGuard],
+  },
   {
     path: 'login',
     component: LoginPageComponent,
