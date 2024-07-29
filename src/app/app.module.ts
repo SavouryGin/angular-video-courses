c25817156c82b2152b6320028413b312103950dc
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2024-07-29 19:55:32 +0200

    feat: install ngrx store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,10 @@ import { CustomRouteReuseStrategy } from './shared/strategies/custom-route-reuse
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AuthInterceptor } from './services/authentication/auth.interceptor';
 import { LoadingBlockComponent } from './components/loading-block/loading-block.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers } from './state/reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -30,6 +34,11 @@ import { LoadingBlockComponent } from './components/loading-block/loading-block.
     BreadcrumbsModule,
     SharedModule,
     HttpClientModule,
+    StoreModule.forRoot(reducers),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode in production
+    }),
   ],
   providers: [
     { provide: RouteReuseStrategy, useClass: CustomRouteReuseStrategy },
---
+++ src/app/state/app.state.ts
@@ -0,0 +1,7 @@
+export interface AppState {
+  // Define your state properties here
+}
+
+export const initialState: AppState = {
+  // Initialize your state properties here
+};
