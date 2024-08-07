73a5398ff8c002e2363bf527bf571c798cc5eb74
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2024-08-07 08:39:58 +0200

    chore: remove obsolete components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../components/button/button.component';
-import { TextInputComponent } from '../components/text-input/text-input.component';
-import { TextAreaComponent } from '../components/text-area/text-area.component';
-import { NumericInputComponent } from '../components/numeric-input/numeric-input.component';
 import { FormTextInputComponent } from '../components/form-text-input/form-text-input.component';
 import { FormDateInputComponent } from '../components/form-date-input/form-date-input.component';
 import { FormNumericInputComponent } from '../components/form-numeric-input/form-numeric-input.component';
@@ -14,9 +11,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     ButtonComponent,
-    TextInputComponent,
-    TextAreaComponent,
-    NumericInputComponent,
     FormFieldComponent,
     FormTextInputComponent,
     FormDateInputComponent,
@@ -26,9 +20,6 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [CommonModule, ReactiveFormsModule],
   exports: [
     ButtonComponent,
-    TextInputComponent,
-    TextAreaComponent,
-    NumericInputComponent,
     FormFieldComponent,
     FormTextInputComponent,
     FormDateInputComponent,
