d8edfac6f56cc451cf5770976658a5c82e6a3570
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2024-09-06 16:48:48 +0200

    test: update unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,28 +1,15 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { TranslateService } from '@ngx-translate/core';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { LogoComponent } from './components/logo/logo.component';
-import { LanguageSelectComponent } from './components/language-select/language-select.component';
-import { of } from 'rxjs';
-import { CUSTOM_ELEMENTS_SCHEMA, Pipe, PipeTransform } from '@angular/core';
 import { provideMockStore } from '@ngrx/store/testing';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-
-@Pipe({ name: 'translate' })
-class MockTranslatePipe implements PipeTransform {
-  transform(value: any): any {
-    return value;
-  }
-}
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 describe('AppComponent', () => {
-  let component: AppComponent;
-  let fixture: ComponentFixture<AppComponent>;
-  let translateService: jasmine.SpyObj<TranslateService>;
-
   beforeEach(async () => {
     const translateSpy = jasmine.createSpyObj('TranslateService', [
       'addLangs',
@@ -33,53 +20,29 @@ describe('AppComponent', () => {
     ]);
 
     await TestBed.configureTestingModule({
-    declarations: [
-        AppComponent,
-        MockTranslatePipe,
-        HeaderComponent,
-        FooterComponent,
-        LogoComponent,
-        LanguageSelectComponent,
-    ],
-    schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    imports: [],
-    providers: [
+      imports: [AppComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {},
+        },
         { provide: TranslateService, useValue: translateSpy },
         provideMockStore({}),
         provideHttpClient(withInterceptorsFromDi()),
         provideHttpClientTesting(),
-    ]
-}).compileComponents();
-
-    fixture = TestBed.createComponent(AppComponent);
-    component = fixture.componentInstance;
-    translateService = TestBed.inject(
-      TranslateService
-    ) as jasmine.SpyObj<TranslateService>;
-
-    // Mock behavior of TranslateService methods
-    translateService.addLangs.and.callFake(() => {});
-    translateService.setDefaultLang.and.callFake(() => {});
-    translateService.use.and.returnValue(of({}));
-
-    fixture.detectChanges();
+      ],
+    }).compileComponents();
   });
 
   it('should create the app', () => {
-    expect(component).toBeTruthy();
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-gmp-2024'`, () => {
-    expect(component.title).toEqual('angular-gmp-2024');
-  });
-
-  it('should call translate service methods on initialization', () => {
-    expect(translateService.addLangs).toHaveBeenCalledOnceWith([
-      'en',
-      'de',
-      'ru',
-    ]);
-    expect(translateService.setDefaultLang).toHaveBeenCalledOnceWith('en');
-    expect(translateService.use).toHaveBeenCalledOnceWith('en');
+  it(`should have the 'angular-routing' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('angular-gmp-2024');
   });
 });
